dab449b3f7d642e9ac5f903057e218c7bcb81f47
Fabrizio Manetti <fmanetti@student.42.fr>
2023-09-25 21:31:34 +0200

    Bootloader uses new logo

--- srcs/client/src/component/layout/root/index.tsx
+++ srcs/client/src/component/layout/root/index.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react';
-import { Outlet }   from 'react-router-dom';
-import BootLoader   from '@/component/boot-loader';
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import BootLoader from "@/component/boot-loader";
 
 // Component -------------------------------------------------------------------
-export default function RootLayout()
-{
-	const [ isBooting, setBooting ] = useState(true);
+export default function RootLayout() {
+  const [isBooting, setBooting] = useState(true);
 
-	return (
-		isBooting
-			? <BootLoader setBooting={setBooting} />
-			: <Outlet />
-	);
+  return isBooting ? <BootLoader setBooting={setBooting} /> : <Outlet />;
 }
