e129849e6735e5bde0566732c8a14c974d7d5d0e
Fabrizio Manetti <fmanetti@student.42.fr>
2023-09-29 15:49:57 +0200

    Added reset_password to login page Added new_password page Still have to do the back for the mail and profile identificaiton

--- srcs/client/src/page/auth/login/index.tsx
+++ srcs/client/src/page/auth/login/index.tsx
@@ -9,30 +9,27 @@ export { action } from "@/feature/auth/login/action";
 export function Component() {
   return (
     <>
-      <div className="black-background relative h-screen overflow-hidden">
-        <div className="ellipse absolute bottom-0 right-0 transform translate-x-1/2 translate-y-1/2 bg-red-600 rounded-full"></div>
-        <div className="ellipse absolute top-0 left-0 transform -translate-x-1/2 -translate-y-1/2 bg-amber-400 rounded-full"></div>
-
-        <div className="flex w-full h-full justify-center">
-          <div className="flex justify-between flex-col items-center w-full">
-            <Link to="/" className="flex justify-center">
-              <img src={MatchaLogo} alt="MatchaLogo" className="w-1/3" />
+      <div className="flex justify-between flex-col items-center w-full">
+        <Link to="/" className="flex justify-center">
+          <img src={MatchaLogo} alt="MatchaLogo" className="w-1/3" />
+        </Link>
+        <div className="flex flex-col h-full justify-center items-center w-1/3">
+          <div className="text-3xl">Welcome back!</div>
+          <LoginForm />
+          <Link
+            to="/auth/reset-password"
+            className="group flex justify-center relative w-full text-white font-semibold py-2 mt-2 rounded-full overflow-hidden border"
+          >
+            Forgot your password?
+          </Link>
+          <div className="flex flex-row justify-center items-center mt-5">
+            You don't have an account?&nbsp;
+            <Link to="/auth/register" className="font-bold underline">
+              Sign Up
             </Link>
-            <div className="flex flex-col h-full justify-center items-center w-1/3">
-              <h4 className="">Welcome back!</h4>
-              <LoginForm />
-              <div className="flex flex-row justify-center items-center mt-5">
-                You don't have an account?&nbsp;
-                <Link to="/auth/register" className="font-bold underline">
-                  Sign Up
-                </Link>
-              </div>
-            </div>
           </div>
         </div>
       </div>
     </>
   );
 }
-
-Component.displayName = "Auth::Login";
