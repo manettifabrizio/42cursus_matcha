caa4728a27d952dc53a9b8852e7066e605e7ffb2
Fabrizio Manetti <fmanetti@student.42.fr>
2024-01-18 11:52:48 +0100

    Fixed login action and likes

--- srcs/client/src/routes/user/profile/likes/index.tsx
+++ srcs/client/src/routes/user/profile/likes/index.tsx
@@ -1,34 +1,59 @@
 import AvailableUsers from '@/component/home/main_page/availableUsers';
-import { useGetUsersQuery } from '@/feature/user/api.slice';
+import {
+	useGetLikesQuery,
+	useLazyGetProfileQuery,
+} from '@/feature/user/api.slice';
 import { Profile } from '@/feature/user/types';
+import { notEmpty } from '@/tool/userTools';
 import { useEffect, useState } from 'react';
 
 export function Component() {
 	const [users, setUsers] = useState<Profile[]>([]);
 	const {
-		data = { users: [] },
-		isFetching,
-		isLoading,
-	} = useGetUsersQuery({ filters: '' });
+		data = { likes: { by_me: [], to_me: [] } },
+		isLoading: isLoadingBlocks,
+		isFetching: isFetchingBlocks,
+	} = useGetLikesQuery();
+	const [
+		getProfile,
+		{ isLoading: isLoadingProfiles, isFetching: isFetchingProfiles },
+	] = useLazyGetProfileQuery();
 
 	useEffect(() => {
-		if (!isFetching && !isLoading) {
-			const concat_users = users.concat(data.users);
-			const users_no_duplicates = Array.from(
-				new Map(concat_users.map((user) => [user.id, user])).values(),
-			);
-			setUsers(users_no_duplicates);
+		if (!(isLoadingBlocks || isFetchingBlocks)) {
+			const likes = data.likes.by_me;
+
+			const getBlockedUsers = async () => {
+				const matchesPromises = likes.map(async (like) => {
+					try {
+						const match = await getProfile({
+							id: like.id_user_to,
+						}).unwrap();
+						return match;
+					} catch (error) {
+						console.error(error);
+						return null;
+					}
+				});
+
+				return Promise.all(matchesPromises);
+			};
+
+			getBlockedUsers().then((res) => {
+				const liked_users = res.filter(notEmpty);
+				setUsers(liked_users);
+			});
 		}
-	}, [data.users, isFetching, isLoading, users]);
+	}, [data, getProfile, isLoadingBlocks, isFetchingBlocks]);
 
 	return (
 		<>
 			<div className="text-3xl mb-3 text-center w-full font-bold">
-				Your Likes
+				Liked Users
 			</div>
 			<AvailableUsers
-				isFetching={isFetching}
-				isLoading={isLoading}
+				isFetching={isFetchingProfiles || isFetchingBlocks}
+				isLoading={isLoadingProfiles || isLoadingBlocks}
 				users={users}
 			/>
 		</>
