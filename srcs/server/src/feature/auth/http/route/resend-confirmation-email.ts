707ded00e4eab5a6a81e57546e9a685404164f82
BOISNIER Thomas <tboisnie@student.42.fr>
2024-02-15 16:25:32 +0100

    Email already confirmed custom error. Resend confirmation feature. Multiple try to fetch CSRF Token.

---
+++ srcs/server/src/feature/auth/http/route/resend-confirmation-email.ts
@@ -0,0 +1,63 @@
+import type { RequestHandler } from "express";
+import * as Config from "@/Config";
+import { service as database_svc } from "@/core/database/service";
+import { service as crypto_svc } from "@/core/cryto/service";
+import { service as mail_svc } from "@/core/mail/service";
+import { service as validation_svc } from "@/core/validation/service";
+import { action as findAccountByCrendentials } from "../../use-case/find-by-credentials/action";
+import { HttpException } from '@/core/exception';
+
+// Type ------------------------------------------------------------------------
+type RequestBody = {
+	username: string
+	password: string
+};
+
+type ResponseBody = void;
+
+// Function --------------------------------------------------------------------
+export const route: RequestHandler<{}, ResponseBody, RequestBody> = async (
+  req,
+  res
+) => {
+    const account = await findAccountByCrendentials(validation_svc, database_svc, crypto_svc, {
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+	if (account === null) {
+		throw new HttpException("Unauthorized", {
+			cause: `Invalid credentials.`
+		});
+	}
+
+	if (account.is_confirmed && (!account.email_new || account.email === account.email_new))
+	{
+		throw new HttpException("Forbidden", {
+			cause: `Email has already been validated.`
+		})
+	}
+
+    // Todo: Match link with frontend routing
+    try {
+		await mail_svc.send({
+			from: '"Matcha" <noreply@matcha.org>',
+			to: account.email_new ?? account.email,
+			subject: "Email Confirmation",
+			html: `
+				Please, click on the following link to validate your email adress: <br>
+				<a href="https://${Config.FRONTEND_HOST}/auth/confirm?id=${account.id}&secret=${account.secret}">
+					Confirm my email
+				</a>
+			`,
+		})
+	}
+	catch (err: any) {
+		throw new HttpException("Failed Dependency", {
+			cause: `Failed to send confirmation email.`,
+			details: err,
+		})
+	}
+
+    return res.status(201).json();
+};
