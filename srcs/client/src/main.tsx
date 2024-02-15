f9a6317e7ba78fed1e16643a1279c5d7799019b9
Fabrizio Manetti <fmanetti@student.42.fr>
2024-02-15 12:03:54 +0100

    Removed ReactStrictMode because is the cause of a lot of "errors"

--- srcs/client/src/main.tsx
+++ srcs/client/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
@@ -8,11 +7,9 @@ import '@/style/global.scss';
 
 // Application -----------------------------------------------------------------
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<div className="w-screen h-screen">
-				<RouterProvider router={router} />
-			</div>
-		</Provider>
-	</React.StrictMode>,
+	<Provider store={store}>
+		<div className="w-screen h-screen">
+			<RouterProvider router={router} />
+		</div>
+	</Provider>,
 );
