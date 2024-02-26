7456f1cf18cae5585bdadffe90d3867be1827c9c
Fabrizio Manetti <fmanetti@student.42.fr>
2024-02-26 17:25:45 +0100

    Fixed overflow in profiles settings menu

--- srcs/client/src/component/home/sidebar/sidebar_main_content/profile_sidebar/menu.tsx
+++ srcs/client/src/component/home/sidebar/sidebar_main_content/profile_sidebar/menu.tsx
@@ -39,31 +39,27 @@ export default function ProfileMenu({ isDesktop }: ProfileMenuProps) {
 
 	return (
 		<div className="flex-1 overflow-auto h-full">
-			<div className="flex flex-col h-full">
-				{!isDesktop && (
-					<div className="text-3xl text-start w-full font-bold mb-3">
-						Profile Settings
-					</div>
-				)}
-				<div className="flex flex-col justify-center items-center h-full">
-					{MenuItems.map((item, index) => (
-						<Link
-							to={item.path}
-							key={index}
-							onClick={() => {
-								dispatch(toggleSidebar(false));
-							}}
-							className={
-								'flex justify-center rounded-xl hover:bg-gray-700 p-2 m-2 ' +
-								(location.pathname === item.path
-									? 'font-bold'
-									: '')
-							}
-						>
-							{item.name}
-						</Link>
-					))}
+			{!isDesktop && (
+				<div className="text-3xl text-start w-full font-bold mb-3">
+					Profile Settings
 				</div>
+			)}
+			<div className="flex flex-col justify-center items-center min-h-full">
+				{MenuItems.map((item, index) => (
+					<Link
+						to={item.path}
+						key={index}
+						onClick={() => {
+							dispatch(toggleSidebar(false));
+						}}
+						className={
+							'flex justify-center rounded-xl hover:bg-gray-700 p-2 m-2 ' +
+							(location.pathname === item.path ? 'font-bold' : '')
+						}
+					>
+						{item.name}
+					</Link>
+				))}
 			</div>
 		</div>
 	);
